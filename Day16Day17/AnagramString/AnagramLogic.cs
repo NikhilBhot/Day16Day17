8deb77ee048700b00b050f915fd617591b00e48f
unknown <bhotnikhil25@gmail.com>
2023-03-31 09:51:05 +0530

    [Nikhil]-Anagram Detection

---
+++ Day16Day17/AnagramString/AnagramLogic.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramString
+{
+    public class AnagramLogic
+    {
+        public static void checkAnagramString(string firstString,string secondString)
+        {
+            //compare lengths of 2 strings
+            if (firstString.Length == secondString.Length)
+            {
+                //convert the strings into character arrays with lowercase
+                char[] ch1 = (firstString.ToLower()).ToCharArray();
+                char[] ch2 = (secondString.ToLower()).ToCharArray();
+
+                //sort the arrays
+                Array.Sort(ch1);
+                Array.Sort(ch2);
+                // convert the character arrays to strings
+                String word1 = new String(ch1);
+                string word2 = new String(ch2);
+                // if two words are equal then 
+                if (word1 == word2)
+                {
+                    Console.WriteLine("Strings {0}  and {1}  are Anagrams ", firstString, secondString);
+                }
+                else
+                {
+                    Console.WriteLine("Strings {0}  and {1}  are Not Anagrams ", firstString, secondString);
+                }
+            }
+            else
+            {//if 2 strings length are not equal
+                Console.WriteLine("Strings {0}  and {1}  are Not Anagrams ", firstString, secondString);
+            }
+        }
+    }
+}
---
+++ Day16Day17/AnagramString/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AnagramString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter the First String");
+            string firstString = Console.ReadLine();
+            Console.WriteLine("Please Enter Second String");
+            string secondString = Console.ReadLine();
+            
+            AnagramLogic.checkAnagramString(firstString, secondString);
+        }
+    }
+}

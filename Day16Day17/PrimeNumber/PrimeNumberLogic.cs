66e3d24919a187bd2c1193226a582aa13df216ed
unknown <bhotnikhil25@gmail.com>
2023-03-31 20:08:04 +0530

    [Nikhil]-Prime Number

---
+++ Day16Day17/PrimeNumber/PrimeNumberLogic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    public class PrimeNumberLogic
+    {
+        public static void findPrimeNumber(int startPos, int endPos)
+        {
+            //Declaration  
+            bool isPrime = true;
+            int i, j;
+            //Calculate and display the Prime number  
+            Console.WriteLine("Prime Numbers are : ");
+            for (i = startPos; i <= endPos; i++)
+            {
+                for (j = 2; j <= 100; j++)
+                {
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.Write("\t" + i);
+                }
+                isPrime = true;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day16Day17/PrimeNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace PrimeNumber
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wel-Come Prime Number Program");
+            Console.WriteLine("Enter The Start Postion From Where You Want To Find Prime Number");
+            int startPos=int.Parse(Console.ReadLine()); 
+            Console.WriteLine("Enter The End Postion From Where You Want To Find Prime Number");
+            int endPos=int.Parse(Console.ReadLine());
+
+            if(startPos==1) 
+            {
+                startPos = startPos + 1;
+            }
+            PrimeNumberLogic.findPrimeNumber(startPos, endPos);
+        }
+    }
+}
